e3b222353feae3e222ce62bce6374d76370fabcd
lukescomputers <lrtrujillo@wpi.edu>
2020-01-23 12:29:52 -0500

    my edits

--- The Boring Commander/boring.c
+++ The Boring Commander/boring.c
@@ -7,20 +7,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
 #include "boring.h"
 
 int main(int argc, char** argv) {
-	execute("whoami");
-	execute("last");
-	execute("ls -al home");
+
+	char *command1[] = {"whoami", NULL};
+	execute(command1);
+
+	char *command2[] = {"last", NULL};
+	execute(command2);
+
+
+	char *command3[] = {"ls", "-al", "/home", NULL};
+	execute(command3);
+
+	
 
 	return 0;
 }
 
-void execute(char* command) {
-	//execute the command and collect information
+void execute(char** command) {
+	pid_t child = fork();
 
-	show(0, 0, 0); //call show with the correct info
+	if(child < 0) {
+		//there is an error
+	} else if(child == 0) { //this is the child process and now call exec
+		
+
+		execvp(command[0], command);
+
+	} else { //this is the parent function
+
+		struct timeval start, end;
+
+
+		gettimeofday(&start, NULL);
+		waitpid(child, NULL, 0);
+		gettimeofday(&end, NULL);
+
+		struct rusage stats;
+
+		getrusage(RUSAGE_CHILDREN, &stats);
+
+		show((end.tv_sec - start.tv_sec), stats.ru_majflt, stats.ru_minflt);
+	}
+
 }
 
 void show(long elaspedTime, int pageFaults, int reclaimedPageFaults) {
--- The Boring Commander/boring.h
+++ The Boring Commander/boring.h
@@ -8,7 +8,7 @@
 #define BORING_H
 	//header stuff going here
 
-	void execute(char* command); //executes the given command, calls show() to display statistics
+	void execute(char** command); //executes the given command, calls show() to display statistics
 
 	void show(long elaspedTime, int pageFaults, int reclaimedPageFaults);
 #endif
